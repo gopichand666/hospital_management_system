3f0dc35e6b11b58c1533c4549d031b5c0532ada4
gopichand666 <gopi831761@gmail.com>
2025-07-31 14:45:35 +0530

    Update app.js

--- hospital-management-system/app.js
+++ hospital-management-system/app.js
@@ -1,5 +1,5 @@
 const PORT = process.env.PORT || 3000;
-mongoose.connect("mongodb://localhost:27017/hms_db", {
+mongoose.connect("mongodb+srv://nskabdul786:nskabdul@hmsdb.vakmxsn.mongodb.net/?retryWrites=true&w=majority&appName=hmsdb, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
