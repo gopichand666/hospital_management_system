3294342a11b9879450c39d9dda57e36935b9f2e4
gopichand666 <gopi831761@gmail.com>
2025-08-01 17:11:12 +0530

    Update main.js

--- hospital-management-system/public/js/main.js
+++ hospital-management-system/public/js/main.js
@@ -1,6 +1,2 @@
 // This file will contain client-side JavaScript for general interactions.
-// Currently, it's empty as navigation is handled by direct links.
-// We'll add more functionality here later for things like form submissions,
-// dynamic content loading, etc., once the backend is set up.
-
 console.log("Hospital Management System Frontend Loaded.");
